38ad0734c04ae2055b7aee0021122da4e957446b
kornrumpfl <luiz.kornrumpf@gmail.com>
2025-05-12 20:03:27 +0200

    PrimeReactProvider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/lara-dark-blue/theme.css";
+import 'primeflex/primeflex.css';  
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "./index.css";
@@ -9,6 +11,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>
 );
